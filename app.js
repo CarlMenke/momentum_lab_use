6cab2835b0c3d4ce91b53eeea0b122abc79906da
carlmenke <carlmenke13@gmail.com>
2022-08-23 07:34:16 -0700

    added randompicture background

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log('hello')
+
+const setBackGourndImage = async () =>{
+//let response = await axios.get('https://api.unsplash.com/photos/random/?client_id=q7_-eEbuDJXNJo1hqmJqkM81oyibAv0rCLoha0SAtHk')
+//document.body.style.backgroundImage = `url('${response.data.urls.regular}')`;
+}
+
+setBackGourndImage();
